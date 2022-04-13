ece72c074bb88ce4068aa4b2701cf4f9caefb297
aislingdeery <aisling.deery@gmail.com>
2022-04-13 10:15:18 -0700

    commit to master

--- React-TS/Embed for your organization/UserOwnsData/src/Config.ts
+++ React-TS/Embed for your organization/UserOwnsData/src/Config.ts
@@ -10,10 +10,10 @@
 export const scopes: string[] = ["https://analysis.windows.net/powerbi/api/Report.Read.All"];
 
 // Client Id (Application Id) of the AAD app.
-export const clientId: string = "";
+export const clientId: string = "5333f332-4da4-4517-a4c6-ab856fd02995";
 
 // Id of the workspace where the report is hosted
-export const workspaceId: string = "";
+export const workspaceId: string = "a0e1f423-37bc-4355-94f4-85463c75b543";
 
 // Id of the report to be embedded
-export const reportId: string = "";
+export const reportId: string = "50a4cca1-cad6-4372-8261-697e4b25fa8e";
